2a052dc23992979365e830fbf8f31177fb5bb568
Kevin Rogers <rogerscardonakevin@gmail.com>
2025-02-15 20:57:53 -0500

    fetch data from .json files in public/data folder, and render topics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import TopicCard from "@/components/TopicCard";
+import { Topic } from "@/models/Topic";
 
 export default function Home() {
+    const [topicsArray, setTopicsArray] = useState<Topic[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(" /data/homeTopics.json");
+                if (!response.ok) {
+                    throw new Error("No se pudo cargar el archivo JSON");
+                }
+                const data: Topic[] = await response.json();
+                setTopicsArray(data);
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div className="mx-5">
             <h1 className="my-10 text-5xl">Bienvenido</h1>
             <h2 className="text-3xl">Unidades:</h2>
             <section className="sm:my-5 grid grid-cols-1 lg:grid-cols-3 gap-x-8 gap-y-4 px-6">
-                <TopicCard title="Unidad 1" explain="" link="/unit1"></TopicCard>
-                <TopicCard title="Unidad 2" explain="" link="/unit2"></TopicCard>
-                <TopicCard title="Unidad 3" explain="" link="/unit3"></TopicCard>
+                {topicsArray.map((topic) => (
+                    <TopicCard key={topic.id} title={topic.title} link={topic.link}></TopicCard>
+                ))}
             </section>
         </div>
     );
--- src/app/unit1/page.tsx
+++ src/app/unit1/page.tsx
@@ -1,15 +1,39 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import TopicCard from "@/components/TopicCard";
+import { Topic } from "@/models/Topic";
+
 export default function Unit1() {
+    const [topicsArray, setTopicsArray] = useState<Topic[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(" /data/unit1/unit1Topics.json");
+                if (!response.ok) {
+                    throw new Error("No se pudo cargar el archivo JSON");
+                }
+                const data: Topic[] = await response.json();
+                setTopicsArray(data);
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <>
             <section className="sm:my-5 grid grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-4 px-6">
-                <TopicCard
-                    title="Programacion Linear"
-                    explain=""
-                    link="unit1/lineal-programming"
-                ></TopicCard>
+                {topicsArray.map((topic) => (
+                    <TopicCard
+                        key={topic.id}
+                        title={topic.title}
+                        explain={topic.explain}
+                        link={topic.link}
+                    ></TopicCard>
+                ))}
             </section>
         </>
     );
