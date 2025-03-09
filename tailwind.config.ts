d90b0b42a417401942daee6a08b482271636756c
Kevin Rogers <rogerscardonakevin@gmail.com>
2025-03-08 22:02:24 -0500

    update tailwind and postcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,49 +1,21 @@
 import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
-import { text } from "stream/consumers";
 
 export default {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                background: "var(--background)",
-                foreground: "var(--foreground)",
-                highlight: "var(--highlight)",
-            },
-            typography: {
-                DEFAULT: {
-                    css: {
-                        h1: {
-                            color: "var(--foreground)",
-                            fontWeight: "700",
-                        },
-                        h2: {
-                            color: "var(--foreground)",
-                            borderBottom: "1px solid var(--foreground)",
-                            borderOpacity: "0.9",
-                        },
-                        strong: {
-                            color: "var(--highlight)",
-                        },
-                        p: {
-                            color: "var(--foreground)",
-                        },
-                        li: {
-                            color: "var(--foreground)",
-                        },
-                        a: {
-                            color: "var(--link)",
-                            textDecoration: "underline",
-                        },
-                    },
-                },
-            },
-        },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        highlight: "var(--highlight)",
+        link: "var(--link)",
+      },
     },
-    plugins: [typography],
+  },
+  plugins: [typography],
 } satisfies Config;
