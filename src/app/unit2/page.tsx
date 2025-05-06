2c4eaab58ee1d6d1673db001c092a560816b584b
Kevin Rogers <rogerscardonakevin@gmail.com>
2025-05-06 15:24:55 -0500

    add content to unit 2 page

--- src/app/unit2/page.tsx
+++ src/app/unit2/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import TopicCard from "@/components/cards/TopicCard";
-import { Topic } from "@/models/Topic";
+import Image from "next/image";
+import TipCard from "@/components/cards/tipCard";
+import Markdown from "@/components/contentComponents/Markdown";
+import Latex from "@/components/contentComponents/Latex";
 
-export default function Unit1() {
-  const [topicsArray, setTopicsArray] = useState<Topic[]>([]);
+export default function SlackVariables() {
+  const [markdownContent, setMarkdownContent] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(" /data/unit2/unit2Topics.json");
+        const response = await fetch("/text/unit2/unit2.md");
         if (!response.ok) {
-          throw new Error("No se pudo cargar el archivo JSON");
+          throw new Error("No se pudo cargar los archivos Markdown");
         }
-        const data: Topic[] = await response.json();
-        setTopicsArray(data);
+        const markdownText: string = await response.text();
+        setMarkdownContent(markdownText);
       } catch (error) {
         console.error("Error:", error);
       }
@@ -23,18 +25,50 @@ export default function Unit1() {
     fetchData();
   }, []);
 
+  const markdownElements = markdownContent.split("---");
+
   return (
-    <>
-      <section className="sm:my-5 grid grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-4 px-6">
-        {topicsArray.map((topic) => (
-          <TopicCard
-            key={topic.id}
-            title={topic.title}
-            explain={topic.explain}
-            link={topic.link}
-          ></TopicCard>
-        ))}
-      </section>
-    </>
+    <div className="mb-20">
+      <Markdown>{markdownElements[0]}</Markdown>
+      <TipCard>{markdownElements[1]}</TipCard>
+      <Markdown>{markdownElements[2]}</Markdown>
+      <div className="flex justify-between items-start gap-x-8">
+        <div className="w-1/2 text-sm">
+          <Markdown>{markdownElements[3]}</Markdown>
+          <Latex
+            content={["X_1 = \\text{cantidad de mesas}", "X_2 = \\text{canridad de sillas}"]}
+          />
+          <Markdown>{markdownElements[4]}</Markdown>
+          <Latex content={["40X_1 + 18X_2 = Z_{max}"]} />
+          <Markdown>{markdownElements[5]}</Markdown>
+          <Latex
+            content={[
+              "16X_1 + 2X_2 \\le 640",
+              "6X_1 + 3X_2 \\le 560",
+              "X_1 \\le 80",
+              "X_2 \\le 120",
+              "X_1, X_2 \\ge 0",
+            ]}
+          />
+        </div>
+        <Image
+          className="m-auto"
+          src="/image/SolverSolution/solverSolution.png"
+          width={600}
+          height={600}
+          alt="Tabla 1"
+        />
+      </div>
+      <Markdown>{markdownElements[6]}</Markdown>
+      <Markdown>{markdownElements[7]}</Markdown>
+      <Image
+        className="m-auto"
+        src="/image/SolverSolution/sensitivityReport.png"
+        width={1000}
+        height={1000}
+        alt="Tabla 1"
+      />
+      <Markdown>{markdownElements[8]}</Markdown>
+    </div>
   );
 }
