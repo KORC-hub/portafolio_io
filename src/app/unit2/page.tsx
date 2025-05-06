31c3569f98bb2b77cfd5db535a24005c6f6ad7ee
Rogers C. <72825328+KORC-hub@users.noreply.github.com>
2025-05-06 22:39:50 +0000

    modify unit 2 page

--- src/app/unit2/page.tsx
+++ src/app/unit2/page.tsx
@@ -26,21 +26,23 @@ export default function SlackVariables() {
   }, []);
 
   const markdownElements = markdownContent.split("---");
+  console.log(markdownElements)
 
   return (
     <div className="mb-20">
       <Markdown>{markdownElements[0]}</Markdown>
       <TipCard>{markdownElements[1]}</TipCard>
       <Markdown>{markdownElements[2]}</Markdown>
+      <Markdown>{markdownElements[3]}</Markdown>
       <div className="flex justify-between items-start gap-x-8">
         <div className="w-1/2 text-sm">
-          <Markdown>{markdownElements[3]}</Markdown>
+          <Markdown>{markdownElements[4]}</Markdown>
           <Latex
             content={["X_1 = \\text{cantidad de mesas}", "X_2 = \\text{canridad de sillas}"]}
           />
-          <Markdown>{markdownElements[4]}</Markdown>
-          <Latex content={["40X_1 + 18X_2 = Z_{max}"]} />
           <Markdown>{markdownElements[5]}</Markdown>
+          <Latex content={["40X_1 + 18X_2 = Z_{max}"]} />
+          <Markdown>{markdownElements[6]}</Markdown>
           <Latex
             content={[
               "16X_1 + 2X_2 \\le 640",
@@ -53,14 +55,21 @@ export default function SlackVariables() {
         </div>
         <Image
           className="m-auto"
-          src="/image/SolverSolution/solverSolution.png"
+          src="/image/SolverSolution/excel.png"
           width={600}
           height={600}
           alt="Tabla 1"
         />
       </div>
-      <Markdown>{markdownElements[6]}</Markdown>
+      <Image
+        className="m-auto"
+        src="/image/SolverSolution/grafico.png"
+        width={1000}
+        height={1000}
+        alt="Tabla 1"
+      />
       <Markdown>{markdownElements[7]}</Markdown>
+      <Markdown>{markdownElements[8]}</Markdown>
       <Image
         className="m-auto"
         src="/image/SolverSolution/sensitivityReport.png"
@@ -68,7 +77,26 @@ export default function SlackVariables() {
         height={1000}
         alt="Tabla 1"
       />
-      <Markdown>{markdownElements[8]}</Markdown>
+      <Markdown>{markdownElements[9]}</Markdown>
+      <div className="ml-10">
+        <Markdown>{markdownElements[10]}</Markdown>
+        <Image
+        className="m-auto"
+        src="/image/SolverSolution/graficoModificado.png"
+        width={700}
+        height={700}
+        alt="Tabla 1"
+      />
+        <Markdown>{markdownElements[11]}</Markdown>
+        <Image
+        className="m-auto"
+        src="/image/SolverSolution/graficoModificado2.png"
+        width={700}
+        height={700}
+        alt="Tabla 1"
+      />
+      </div>
+      <Markdown>{markdownElements[12]}</Markdown>
     </div>
   );
 }
