658252b2407628c8f5e79018db7e7a4e9cb5fdb9
Kevin Rogers <rogerscardonakevin@gmail.com>
2025-05-06 15:26:02 -0500

    edit text size

--- src/components/contentComponents/Latex.tsx
+++ src/components/contentComponents/Latex.tsx
@@ -7,6 +7,6 @@ type Props = {
 export default function SubSection(props: Props) {
     const content = TextArrayToLatex(props.content);
     return (
-        <p className="ml-10 text-sm md:text-lg lg:text-xl" dangerouslySetInnerHTML={{ __html: content }}></p>
+        <p className="ml-10 text-sm md:text-lg lg:text-sm" dangerouslySetInnerHTML={{ __html: content }}></p>
     );
 }
